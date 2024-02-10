0a0195e320d79a69a1701978bf862e724150e0b5
guyvinay <mrsingvinay563@gmail.com>
2024-02-10 15:23:59 +0530

    CSV-FILE-OPS-code-comment-acc

--- Rest-Template/Prospecta/CSVOps/src/com/app/Main.java
+++ Rest-Template/Prospecta/CSVOps/src/com/app/Main.java
@@ -1,6 +1,7 @@
 package com.app;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,8 +16,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-        try {
+            //declaring method to read  values from CSV file
             Map<String, String> values = readCSV(inputFilePath);
+
 
             System.out.println(values);
 
@@ -27,35 +29,52 @@ public class Main {
 
             //Writing processed value to CSV file
             writeToCSVFile(ans);
-        } catch (IOException e) {
-            System.err.println("Error processing CSV file: " + e.getMessage());
-        }
+
+
 	}
 
-    public static Map<String, String> readCSV(String filePath) throws IOException {
+    public static Map<String, String> readCSV(String filePath) {
 
+        //declaring LinkedHashMap to store read entries from CSV file in order
         Map<String, String> values = new LinkedHashMap<>();
-        
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath)))  {
+
+        //Try with resources to handle File Reader likely exceptions
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+
+            //Reading lines from file
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                for(String str: parts){
+
+                //split values with ,
+                String[] entries = line.split(",");
+                for(String str: entries){
+                    //getting key-value pair as : divides key-value in csv
                     String[] vals = str.split(":");
+
+                    //storing literals as key and value as value in Map
                     values.put(vals[0].trim(), vals[1].trim());
                 }
             }
+        } catch (IOException e) {
+            System.out.println("Reading CSV file failed!!! "+ e.getMessage());
         }
         return values;
     }
 
+    //Processing Extracted map from CSV file to do airthmatic ops
 	public static Map<String, Double> processMap(Map<String, String> values) {
+
+        //Map to store airthematice operated entries in map
         Map<String, Double> ansMap = new LinkedHashMap<>();
+
+        //Iterating throught it
         for(Map.Entry<String, String> ent:values.entrySet()){
             String key = ent.getKey();
             String val = ent.getValue();
+
+            //if value starts with '=' perform airthematic ops on that entry
             if(val.startsWith("=")){
-                double sum = evaluateValue(val.substring(1), values);
+                double sum = evaluateValue(val.substring(1), ansMap);
                 ansMap.put(key, sum);
             }else{
                 ansMap.put(key, Double.parseDouble(val));
@@ -64,29 +83,31 @@ public class Main {
         return ansMap;
     }
 
-    public static double evaluateValue(String str, Map<String, String> values) {
+    //Calculating sum when encountered '='
+    public static double evaluateValue(String str, Map<String, Double> values) {
+        //Split expression 'A+B' so Am B values can be get
         String[] vals = str.split("\\+");
         double sum = 0;
         for(String val: vals){
             if(values.containsKey(val)){
-                // System.out.println(values.get(val));
+                sum+= values.get(val);
             }
             else sum+=Double.parseDouble(val);
         }
-        // System.out.println(sum);
         return sum;
     }
-
     
+    //Writing to CSV file after processing retrieved data
     public static void writeToCSVFile(Map<String, Double> ans) {
-        try (FileWriter fileWriter = new FileWriter(outputFilePath)) {
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFilePath))) {
+            
             for(Map.Entry<String, Double> ent : ans.entrySet()){
-                fileWriter.append(ent.getKey()).append(":").append(String.valueOf(ent.getValue())).append(",\n");
+                bufferedWriter.append(ent.getKey()).append(":").append(String.valueOf(ent.getValue())).append(",\n");
             }
         } catch (IOException e) {
            System.out.println("Write to CSV file failed: "+e.getMessage());
         }
     }
-
 }
 
