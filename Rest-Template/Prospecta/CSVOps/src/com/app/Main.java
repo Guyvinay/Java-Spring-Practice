c66ffebadff0969714cb79eed6e391c7416fb225
guyvinay <mrsingvinay563@gmail.com>
2024-02-10 12:58:12 +0530

    CSV-FILE-OPS

--- Rest-Template/Prospecta/CSVOps/src/com/app/Main.java
+++ Rest-Template/Prospecta/CSVOps/src/com/app/Main.java
@@ -16,7 +16,8 @@ public class Main {
         try {
             Map<String, String> values = readCSV(inputFilePath);
             System.out.println(values);
-            processMap(values);
+            Map<String, Double> ans = processMap(values);
+            System.out.println(ans);
         } catch (IOException e) {
             System.err.println("Error processing CSV file: " + e.getMessage());
         }
@@ -24,53 +25,44 @@ public class Main {
 
     private static Map<String, String> readCSV(String filePath) throws IOException {
         Map<String, String> values = new LinkedHashMap<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath)))  {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
                 for(String str: parts){
                     String[] vals = str.split(":");
-                    vals[1] = vals[1].trim();
-                    vals[0] = vals[0].trim();
-                    values.put(vals[0], vals[1]);
-                    // System.out.println(Arrays.toString(vals));
-
+                    values.put(vals[0].trim(), vals[1].trim());
                 }
             }
         }
         return values;
     }
 
-	private static void processMap(Map<String, String> values) {
+	private static Map<String, Double> processMap(Map<String, String> values) {
         Map<String, Double> ansMap = new LinkedHashMap<>();
         for(Map.Entry<String, String> ent:values.entrySet()){
             String key = ent.getKey();
             String val = ent.getValue();
             if(val.startsWith("=")){
-                // System.out.println(val);
-                double sum = evaluateValue(val, values);
+                double sum = evaluateValue(val.substring(1), values);
                 ansMap.put(key, sum);
             }else{
                 ansMap.put(key, Double.parseDouble(val));
             }
         }
-        System.out.println(ansMap);
+        return ansMap;
     }
 
     private static double evaluateValue(String str, Map<String, String> values) {
-        str = str.substring(1);
         String[] vals = str.split("\\+");
-        // System.out.println(Arrays.toString(vals));
         double sum = 0;
-        
         for(String val: vals){
             if(values.containsKey(val)){
-                // sum+= values.get(val);
-                System.out.println(values.get(val));
+                // System.out.println(values.get(val));
             }
             else sum+=Double.parseDouble(val);
         }
-
+        // System.out.println(sum);
         return sum;
     }
 
